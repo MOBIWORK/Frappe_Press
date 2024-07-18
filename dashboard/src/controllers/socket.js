0202441de4ddca0ae4d7977fbe922dc4c328357b
aedudon <nghienbui990@gmail.com>
2024-07-18 09:47:24 +0700

    update dockerfile

--- dashboard/src/controllers/socket.js
+++ dashboard/src/controllers/socket.js
@@ -5,8 +5,8 @@ let host = window.location.hostname;
 let port = window.location.port ? `:${config.socketio_port}` : '';
 let protocol = port ? 'http' : 'https';
 let siteName = window.site_name || host;
-// let url = `${protocol}://${host}${port}/${siteName}`;
-let url = `${protocol}://${host}${port}`;
+let url = `${protocol}://${host}${port}/${siteName}`;
+//let url = `${protocol}://${host}${port}`;
 let socket = io(url, {
 	withCredentials: true
 });
